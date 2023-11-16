ef6bb1a591a93292adb6b62014ee1d3a6855769a
UsuarioM <oskitarpereal@gmail.com>
2023-11-16 13:32:23 +0100

    modificado modelo

--- src/api/models/employee.model.js
+++ src/api/models/employee.model.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const EmployeeSchema = new mongoose.Schema(
   {
     emplid: { type: Number, required: true, unique: true },
-    firstname: { type: String, required: true, trim: true },
+    name: { type: String, required: true, trim: true },
     lastname: { type: String, required: true, trim: true },
     nif: { type: String, required: true, trim: true },
     phone: { type: String, required: true, trim: true },
@@ -24,11 +24,10 @@ const EmployeeSchema = new mongoose.Schema(
         "otros",
       ],
     },
-    guys: { type: Number, enum: [0, 1, 2, 3, 4] },
-    girls: { type: Number, enum: [0, 1, 2, 3, 4] },
+    children: { type: Number, enum: [0, 1, 2, 3, 4] },
     cif: { type: String, required: true, trim: true },
     company: { type: String, trim: true },
-    segsocnro: { type: String, required: true, trim: true },
+    ssnumber: { type: String, required: true, trim: true },
     addresses: { type: String, required: true, trim: true },
     department: { type: String, required: true, trim: true },
     laborInformation: [
